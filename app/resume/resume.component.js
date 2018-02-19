9d7c0533d0d89235bc163d34f7cf2c457c0ebef4
Estevao Machado <estevaom@gmail.com>
2018-02-19 10:43:50 -0300

    Improve mobile layout

--- app/resume/resume.component.js
+++ app/resume/resume.component.js
@@ -13,7 +13,7 @@ resume.component('resume', {
       self.technologies = [
         { type: 'Backend', handlers: ['ruby-on-rails', 'node-js'] },
         { type: 'Frontend', handlers: ['angularjs', 'angular-4', 'bootstrap', 'css', 'google-material', 'html5', 'javascript', 'jquery'] },
-        { type: 'Database', handlers: ['mysql', 'postgresql', 'amazon-dynamodb', 'amazon-redshift', 'treasure-data'] },
+        { type: 'Database', handlers: ['mysql', 'postgresql', 'amazon-dynamodb', 'amazon-redshift', 'treasure-data', 'google-cloud-datastore'] },
         { type: 'Cache Storage', handlers: ['elasticsearch', 'memcached', 'redis'] },
         { type: 'Test', handlers: ['capybara', 'jasmine', 'karma', 'minitest', 'mocha', 'protractor', 'rspec'] },
         { type: 'CI/CD', handlers: ['jenkins', 'semaphore', 'wercker'] },
