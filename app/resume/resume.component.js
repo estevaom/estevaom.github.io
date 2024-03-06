1072dd8e08fcfda41e7ea23fb6dc0caffc5c8e5b
Estevão de Abreu Machado <estevaom@gmail.com>
2024-03-06 00:20:59 +0000

    Add Readytech employment

--- app/resume/resume.component.js
+++ app/resume/resume.component.js
@@ -21,12 +21,12 @@
 
         self.technologies = [
             { type: 'Backend', handlers: ['ruby-on-rails', 'node-js'] },
-            { type: 'Frontend', handlers: ['angularjs', 'angular-4', 'bootstrap', 'css', 'google-material', 'html5', 'javascript', 'jquery'] },
+            { type: 'Frontend', handlers: ['angularjs', 'angular-4', 'bootstrap', 'css', 'google-material', 'html5', 'hotwire', 'javascript', 'jquery'] },
             { type: 'Database', handlers: ['mysql', 'postgresql', 'amazon-dynamodb', 'amazon-redshift', 'treasure-data', 'google-cloud-datastore'] },
             { type: 'Cache Storage', handlers: ['elasticsearch', 'memcached', 'redis'] },
             { type: 'Test', handlers: ['capybara', 'jasmine', 'karma', 'minitest', 'mocha', 'protractor', 'rspec'] },
             { type: 'CI/CD', handlers: ['jenkins', 'semaphore', 'wercker'] },
-            { type: 'Other', handlers: ['amazon-s3', 'capistrano', 'datamapper', 'git', 'new-relic', 'sidekiq', 'typescript', 'vagrant', 'webpack'] }
+            { type: 'Other', handlers: ['amazon-s3', 'capistrano', 'datamapper', 'git', 'github-copilot', 'new-relic', 'sidekiq', 'typescript', 'vagrant', 'webpack'] }
         ];
     }
 })(angular);
