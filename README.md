8ccf844c8c514af76cb5f7e0e69f9f360fff736e
Estevao Machado <estevao.machado@reinteractive.com>
2025-02-01 23:30:00 +1000

    feat: add lightbox for blog post images and update README
    - Add lightbox functionality to blog post images
    - Make images clickable to show in fullscreen
    - Add ESC key support to close lightbox
    - Update README with comprehensive list of tools used
    - Organize tools into categories (editors, AI, frameworks)

---
+++ README.md
@@ -0,0 +1,20 @@
+# estevaom.github.io
+
+My personal website and blog.
+
+## Created using:
+
+### Code Editors
+- Windsurf
+- Visual Studio Code
+- Trae
+
+### AI Assistants
+- Claude 3.5-sonnet
+- GitHub Copilot
+- Cline
+
+### Frameworks and Libraries
+- AngularJS
+- Material Design Lite
+- Tailwind CSS
--- blog/app/blog-post/blog-post.component.js
+++ blog/app/blog-post/blog-post.component.js
@@ -13,9 +13,28 @@
             controller: BlogPostController
         });
 
-    function BlogPostController() {
+    function BlogPostController($scope, $window) {
         var self = this;
         var starsCache = null;
+        var escHandler = null;
+
+        self.$onInit = function() {
+            self.showLightbox = false;
+            escHandler = function(event) {
+                if (event.key === 'Escape' && self.showLightbox) {
+                    $scope.$apply(function() {
+                        self.closeLightbox();
+                    });
+                }
+            };
+            angular.element($window).on('keydown', escHandler);
+        };
+
+        self.$onDestroy = function() {
+            if (escHandler) {
+                angular.element($window).off('keydown', escHandler);
+            }
+        };
 
         self.$onChanges = function(changes) {
             if (changes.post) {
@@ -31,6 +50,14 @@
                 });
             }
             return starsCache;
+        };
+
+        self.openLightbox = function() {
+            self.showLightbox = true;
+        };
+
+        self.closeLightbox = function() {
+            self.showLightbox = false;
         };
     }
 })(angular);
