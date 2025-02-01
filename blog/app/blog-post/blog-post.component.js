97bafb5c733edcf59b692d50841cedb719b88493
Estevao Machado <estevao.machado@reinteractive.com>
2025-02-02 00:22:14 +1000

    Add image carousel in lightbox and update editor descriptions
    - Add support for multiple images per post
    - Move carousel functionality to lightbox view
    - Add keyboard navigation for images
    - Update Trae's UI description
    - Add multiple images for Windsurf and Trae editors

--- blog/app/blog-post/blog-post.component.js
+++ blog/app/blog-post/blog-post.component.js
@@ -20,10 +20,23 @@
 
         self.$onInit = function() {
             self.showLightbox = false;
+            self.currentImageIndex = 0;
+            self.currentImage = self.post.images[0];
+
             escHandler = function(event) {
-                if (event.key === 'Escape' && self.showLightbox) {
+                if (!self.showLightbox) return;
+                
+                if (event.key === 'Escape') {
                     $scope.$apply(function() {
                         self.closeLightbox();
+                    });
+                } else if (event.key === 'ArrowLeft' && self.post.images.length > 1) {
+                    $scope.$apply(function() {
+                        self.prevImage(event);
+                    });
+                } else if (event.key === 'ArrowRight' && self.post.images.length > 1) {
+                    $scope.$apply(function() {
+                        self.nextImage(event);
                     });
                 }
             };
@@ -39,7 +52,41 @@
         self.$onChanges = function(changes) {
             if (changes.post) {
                 starsCache = null;
+                self.currentImageIndex = 0;
+                self.currentImage = self.post.images[0];
             }
+        };
+
+        self.nextImage = function($event) {
+            if ($event) {
+                $event.stopPropagation();
+            }
+            self.currentImageIndex = (self.currentImageIndex + 1) % self.post.images.length;
+            self.currentImage = self.post.images[self.currentImageIndex];
+        };
+
+        self.prevImage = function($event) {
+            if ($event) {
+                $event.stopPropagation();
+            }
+            self.currentImageIndex = (self.currentImageIndex - 1 + self.post.images.length) % self.post.images.length;
+            self.currentImage = self.post.images[self.currentImageIndex];
+        };
+
+        self.setImage = function(index, $event) {
+            if ($event) {
+                $event.stopPropagation();
+            }
+            self.currentImageIndex = index;
+            self.currentImage = self.post.images[self.currentImageIndex];
+        };
+
+        self.handleLightboxClick = function($event) {
+            if ($event.target.tagName.toLowerCase() === 'img' ||
+                $event.target.tagName.toLowerCase() === 'button') {
+                return;
+            }
+            self.closeLightbox();
         };
 
         self.getStars = function() {
